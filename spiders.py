25c4e256961f39294deee6b14ec229e51be8d15b
archie2k16 <me@archie.cc>
2019-12-24 05:30:26 +0800

    added stockbased

--- spiders.py
+++ spiders.py
@@ -1,20 +1,23 @@
-from utils import SpiderTimeBased, pro
+from utils import SpiderTimeBased, SpiderStokeBased, pro
 
 
 class DailyBasic(SpiderTimeBased):
     pickle_path = 'raw/daily_basic'
     pkl_prefix = 'daily_basic_'
 
-    def fetch(date):
-        df = pro.daily_basic(trade_date=date)
+    @staticmethod
+    def fetch(val):
+        df = pro.daily_basic(trade_date=val)
         return df
+
 
 class DayTick(SpiderTimeBased):
     pickle_path = 'raw/day_tick'
     pkl_prefix = 'day_tick_'
 
-    def fetch(date):
-        df = pro.daily(trade_date=date)
+    @staticmethod
+    def fetch(val):
+        df = pro.daily(trade_date=val)
         return df
 
 
@@ -22,11 +25,86 @@ class MoneyFlow(SpiderTimeBased):
     pickle_path = 'raw/money_flow'
     pkl_prefix = 'money_flow_'
 
-    def fetch(date):
-        df = pro.moneyflow(trade_date=date)
+    @staticmethod
+    def fetch(val):
+        df = pro.moneyflow(trade_date=val)
         return df
 
+
+class NameChange(SpiderStokeBased):
+    pickle_path = 'raw/name_change'
+    pkl_prefix = 'name_change_'
+
+    @staticmethod
+    def fetch(val):
+        df = pro.namechange(ts_code=val)
+        return df
+
+
+class NameChange(SpiderStokeBased):
+    pickle_path = 'raw/name_change'
+    pkl_prefix = 'name_change_'
+
+    @staticmethod
+    def fetch(val):
+        df = pro.namechange(ts_code=val)
+        return df
+
+
+class NameChange(SpiderStokeBased):
+    pickle_path = 'raw/name_change'
+    pkl_prefix = 'name_change_'
+
+    @staticmethod
+    def fetch(val):
+        df = pro.namechange(ts_code=val)
+        return df
+
+
+class NameChange(SpiderStokeBased):
+    pickle_path = 'raw/name_change'
+    pkl_prefix = 'name_change_'
+
+    @staticmethod
+    def fetch(val):
+        df = pro.namechange(ts_code=val)
+        return df
+
+
+class NameChange(SpiderStokeBased):
+    pickle_path = 'raw/name_change'
+    pkl_prefix = 'name_change_'
+
+    @staticmethod
+    def fetch(val):
+        df = pro.namechange(ts_code=val)
+        return df
+
+
+class NameChange(SpiderStokeBased):
+    pickle_path = 'raw/name_change'
+    pkl_prefix = 'name_change_'
+    update_limit = 500
+
+    @staticmethod
+    def fetch(val):
+        df = pro.namechange(ts_code=val)
+        return df
+
+
+class FinanceAudit(SpiderStokeBased):
+    pickle_path = 'raw/finance_audit'
+    pkl_prefix = 'finance_audit_'
+    update_limit = 400
+
+    @staticmethod
+    def fetch(val):
+        df = pro.fina_audit(ts_code=val)
+        return df
+
+
 if __name__ == '__main__':
-    DailyBasic.update()
-    DayTick.update()
-    MoneyFlow.update()
+    # DailyBasic.update()
+    # DayTick.update()
+    # MoneyFlow.update()
+    FinanceAudit.update()
--- utils.py
+++ utils.py
@@ -1,11 +1,17 @@
-import os, re, datetime, pickle
+import os, re, datetime, pickle, time
 import tushare as ts
 from conf import ts_token, start_date
-from abc import abstractmethod
 import termcolor
 
 
 def logprop(color, title):
+    '''
+    带颜色格式输出装饰器
+    :param color: 颜色
+    :param title: 标志
+    :return:
+    '''
+
     def wrapper(func):
         def wrapped(msg):
             termcolor.cprint(f'{title}:{msg}', color=color)
@@ -16,6 +22,10 @@ def logprop(color, title):
 
 
 class log(object):
+    '''
+    格式输出类
+    分为四个级别：INFO、MSG、WARNING和ERROR
+    '''
 
     @logprop('green', 'INFO')
     @staticmethod
@@ -43,6 +53,13 @@ pro = ts.pro_api()
 
 
 def end_date():
+    '''
+    用来获取最后的交易日期
+    如果当前时间超过下午3点，获取当前日期
+    反之获取前一天
+    :return:
+    'yyyymmdd'格式的字符串日期
+    '''
     now = datetime.datetime.now()
     hour = now.hour
     if hour >= 15:
@@ -61,47 +78,96 @@ end_date = end_date()
 
 
 def trade_cal():
+    '''
+    获取交易日历
+    如果已获取最新的交易日历，则直接读取数据，否则从线上获取并储存
+    :return:
+    '''
     pickle_path = 'raw/common'
     file_name = 'trade_cal.pkl'
     file_path = os.path.join(pickle_path, file_name)
+    # 判断路径是否存在
     if os.path.exists(pickle_path):
+        # 判断文件是否存在
         if os.path.isfile(file_path):
-            with open(os.path.join(pickle_path, file_name), 'rb') as f:
-                trade_date = pickle.load(f)
-                if max(trade_date) == end_date:
+            with open(file_path, 'rb') as f:
+                dstore = pickle.load(f)
+                trade_date = dstore['trade_date']
+                # 判断是否最近更新
+                if dstore['last_update'] == end_date:
                     return trade_date
-
     else:
         os.makedirs(pickle_path)
-
     online_date = pro.trade_cal(start_date=start_date, end_date=end_date)
     td = online_date[online_date['is_open'] == 1]
     trade_date = set(td['cal_date'])
     with open(os.path.join(pickle_path, file_name), 'wb') as f:
-        pickle.dump(trade_date, f)
+        dstore = {'last_update': end_date,
+                  'trade_date': trade_date}
+        pickle.dump(dstore, f)
     return trade_date
 
 
 def stock_list():
+    '''
+    获取股票列表
+    :return:
+    '''
     pickle_path = 'raw/common'
     file_name = 'stock_list.pkl'
     file_path = os.path.join(pickle_path, file_name)
+    # 判断路径是否存在
     if os.path.exists(pickle_path):
+        # 判断文件是否存在
         if os.path.isfile(file_path):
-            with open(os.path.join(pickle_path, file_name), 'rb') as f:
-                trade_date = pickle.load(f)
-                if max(trade_date) == end_date:
-                    return trade_date
-
+            with open(file_path, 'rb') as f:
+                dstore = pickle.load(f)
+                trade_date = trade_cal()
+                # 判断是否最近更新
+                if dstore['last_update'] == end_date:
+                    return dstore['stock_list']
+                # 如果end_date为非交易日
+                elif dstore['last_update'] >= max(trade_date):
+                    f.close()
+                    with open(file_path, 'wb') as f:
+                        dstore['last_update'] = end_date
+                        pickle.dump(dstore, f)
+                else:
+                    f.close()
+                    from spiders import DayTick
+                    DayTick.check()
+                    rv = set()
+                    cpoint = f'{DayTick.pkl_prefix}{dstore["last_update"]}{DayTick.pkl_sufix}'
+                    for i in os.listdir(DayTick.pickle_path):
+                        if i.startswith(DayTick.pkl_prefix) and i.endswith(
+                                DayTick.pkl_sufix) and i > cpoint:
+                            with open(os.path.join(DayTick.pickle_path, i), 'rb') as f:
+                                df = pickle.load(f)
+                                tscode = set(df['ts_code'])
+                                rv = rv | tscode
+                    with open(os.path.join(pickle_path, file_name), 'wb') as f:
+                        dstore = {'last_update': end_date,
+                                  'stock_list': rv}
+                        pickle.dump(dstore, f)
     else:
         os.makedirs(pickle_path)
-
-    online_date = pro.trade_cal(start_date=start_date, end_date=end_date)
-    td = online_date[online_date['is_open'] == 1]
-    trade_date = set(td['cal_date'])
+    # todo 此处循环引用，降低代码复用效率，暂时没有更好的解决方案
+    from spiders import DayTick
+    req = DayTick.check()
+    rv = set()
+    if req['diff']:
+        DayTick.update()
+    for i in os.listdir(DayTick.pickle_path):
+        if i.startswith(DayTick.pkl_prefix) and i.endswith(DayTick.pkl_sufix):
+            with open(os.path.join(DayTick.pickle_path, i), 'rb') as f:
+                df = pickle.load(f)
+                tscode = set(df['ts_code'])
+                rv = rv | tscode
     with open(os.path.join(pickle_path, file_name), 'wb') as f:
-        pickle.dump(trade_date, f)
-    return trade_date
+        dstore = {'last_update': end_date,
+                  'stock_list': rv}
+        pickle.dump(dstore, f)
+    return rv
 
 
 class Spider(object):
@@ -121,7 +187,7 @@ class Spider(object):
         pass
 
     @staticmethod
-    def fetch(date):
+    def fetch(val):
         pass
 
     def to_sql(self):
@@ -175,8 +241,55 @@ class SpiderTimeBased(Spider):
 
 
 class SpiderStokeBased(Spider):
-    pass
+    update_limit = 100
+
+    @classmethod
+    def check(cls):
+        if os.path.exists(cls.pickle_path):
+            file_list = [i for i in os.listdir(cls.pickle_path) if
+                         i.startswith(cls.pkl_prefix) and i.endswith(cls.pkl_sufix)]
+            rep = f'{cls.pkl_prefix}(\d+\.\w+)\{cls.pkl_sufix}'
+            pattern = re.compile(rep)
+            exist_stocks = set([pattern.findall(i)[0] for i in file_list])
+
+        else:
+            os.makedirs(cls.pickle_path)
+            exist_stocks = set()
+        slist = stock_list()
+        diff = slist - exist_stocks
+
+        rv = {'online_data': len(slist),
+              'local_data': len(exist_stocks),
+              'diff': diff
+              }
+        return rv
+
+    @classmethod
+    def update(cls):
+        log.info(f'UPDATING {cls.__name__}')
+        req = cls.check()
+        diff = req['diff']
+        length = len(diff)
+        if diff:
+            log.info(f'{len(diff)} dataset(s) to update')
+            t0 = t1 = time.time()
+            for i, item in enumerate(diff):
+                df = cls.fetch(item)
+                file_name = f'{cls.pkl_prefix}{item}{cls.pkl_sufix}'
+                file_path = os.path.join(cls.pickle_path, file_name)
+                with open(file_path, 'wb') as f:
+                    pickle.dump(df, f)
+                log.info(f'{file_name} updated')
+                deltaT = time.time() - t0
+                log.msg(f'{i + 1} out of {length} finished, time elapsed {round(deltaT, 2)}s,'
+                        f'remaining {round(deltaT * (length - i) / (i + 1), 2)}s')
+                if i / deltaT > (cls.update_limit*0.9 / 60):
+                    log.warn('TOO FAST, SLEEP FOR A WHILE')
+                    time.sleep(1)
+        else:
+            log.info(f'data updated to the latest')
 
 
 if __name__ == '__main__':
-    log.info('helo')
+    print(stock_list())
+    print(len(stock_list()))
